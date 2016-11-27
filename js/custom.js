2043c21746dc1b7a00078032926ea3b046c82a06
Alexandr Gorchakov <385898sw@gmail.com>
2016-11-27 10:30:59 +0600

    first commit

---
+++ js/custom.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $.localScroll();
+    
+    $.extend($.scrollTo.defaults, {        
+        offset: -70
+    }); 
+        
+    $(".scroll-to-item").click( function() {       
+       $(window).scrollTo( $(this).attr('href'), 1000, {offset: {top:-70, left:0} });       
+    });
+    
+    $(".navbar-toggle").click( function() {       
+        $(".white-background").toggle();
+    });
+    
+    $(".nav-link").click( function() {       
+        $(".white-background").toggle();
+    });
+});
